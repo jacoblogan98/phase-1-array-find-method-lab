3d2dc14b52b024754f0124c0d570094864bb5884
Jacob Logan <jacoblogan98@gmail.com>
2022-03-16 16:19:26 -0400

    commit tree f4674aacf32efd9c009c2761c6654f150fe4d08f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+
+function superbowlWin(record) {
+    let winningYear = record.find(record => record.result === "W");
+     if (winningYear) {
+        return winningYear.year;
+    }
+}
